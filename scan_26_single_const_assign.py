53f908d8bf85f1fd2da825fef9ea4e71058b9502
parker-research <166864283+parker-research@users.noreply.github.com>
2025-03-31 18:23:16 +0200

    Create argparse searcher

--- scan_26_single_const_assign.py
+++ scan_26_single_const_assign.py
@@ -4,8 +4,9 @@ We will flag any such cases.
 """
 
 import pyslang
-# import argparse
+import argparse
 from typing import Literal
+from collections.abc import Iterator
 import re
 
 from pathlib import Path
@@ -30,9 +31,11 @@ def clean_verilog_comments(verilog: str) -> str:
     verilog = re.sub(r"/\*.*?\*/", "", verilog, flags=re.DOTALL)
     return verilog
 
+
 VERILOG_TYPE_LITERAL_t = Literal[
     "logic", "wire", "input reg", "input wire", "output reg", "output wire", "reg"
 ]
+
 
 def extract_declared_identifiers(
     verilog: str,
@@ -67,7 +70,15 @@ def extract_declared_identifiers(
         ]
 
         assert isinstance(var_type, str)
-        assert var_type in ("logic", "wire", "input reg", "input wire", "output reg", "output wire", "reg")
+        assert var_type in (
+            "logic",
+            "wire",
+            "input reg",
+            "input wire",
+            "output reg",
+            "output wire",
+            "reg",
+        )
 
         if var_type not in output:
             output[var_type] = []
@@ -98,11 +109,14 @@ def count_constant_assignments_to_identifier(verilog: str, identifier: str) -> i
 
     return count
 
-def find_all_single_constant_assignments_in_verilog(verilog: str) -> list[tuple[str, str]]:
+
+def find_all_single_constant_assignments_in_verilog(
+    verilog: str,
+) -> list[tuple[str, str]]:
     """Find all identifiers with single constant assignments."""
     verilog = clean_verilog_comments(verilog)
     identifiers = extract_declared_identifiers(verilog)
-    
+
     # Tuple of (identifier name, identifier type)
     results: list[tuple[str, str]] = []
 
@@ -113,3 +127,60 @@ def find_all_single_constant_assignments_in_verilog(verilog: str) -> list[tuple[
                 results.append((identifier, var_type))
 
     return results
+
+
+def find_all_single_constant_assignments_in_project(
+    project_folder_path: Path,
+) -> Iterator[tuple[str, str, str]]:
+    """Find all identifiers with single constant assignments in a project folder.
+
+    Returns a list of tuples containing the identifier name, type, and relative file path.
+    """
+
+    for file_path in list(project_folder_path.rglob("*.v")) + list(
+        project_folder_path.rglob("*.sv")
+    ):
+        verilog = file_path.read_text()
+        single_constant_assignments = find_all_single_constant_assignments_in_verilog(
+            verilog
+        )
+        for identifier, var_type in single_constant_assignments:
+            yield (
+                identifier,
+                var_type,
+                file_path.relative_to(project_folder_path).as_posix(),
+            )
+
+
+def main():
+    # Create an arg parser with one argument: project folder path.
+    parser = argparse.ArgumentParser(
+        description="Find single constant assignments in Verilog files."
+    )
+    parser.add_argument(
+        "project_folder_path",
+        type=str,
+        help="Path to the project folder containing Verilog files.",
+    )
+    args = parser.parse_args()
+    project_folder_path = Path(args.project_folder_path)
+    if not project_folder_path.is_dir():
+        raise ValueError(f"{project_folder_path} is not a valid directory.")
+    if not project_folder_path.exists():
+        raise ValueError(f"{project_folder_path} does not exist.")
+
+    # Find all single constant assignments in the project folder.
+    results: list[tuple[str, str, str]] = []
+    for (
+        identifier,
+        var_type,
+        file_path,
+    ) in find_all_single_constant_assignments_in_project(project_folder_path):
+        print(f"Identifier: {identifier}, Type: {var_type}, File: {file_path}")
+        results.append((identifier, var_type, file_path))
+
+    print(f"Found {len(results)} single constant assignments.")
+
+
+if __name__ == "__main__":
+    main()
